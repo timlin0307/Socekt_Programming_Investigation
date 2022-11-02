cce9bf05be939183f81fe81247a5b40d6c07353a
Sasaki Yamato <54052564+timlin0307@users.noreply.github.com>
2022-11-03 00:48:18 +0800

    Add files via upload

--- Q3/udp_client.py
+++ Q3/udp_client.py
@@ -13,26 +13,28 @@ sock.bind((addr, port_self))
 t_start = time.time()
 
 for i in range(10000):
-    if(i % 2 == 0):
+    if (i % 2 == 0):
         sock.settimeout(1)
     else:
         sock.settimeout(0.005)
+
     data = b'Hello %d' % (i+1)
     sock.sendto(data, (addr, port_destin))
     print("Send", data, "to proxy")
+
     while True:
         try:
             # 32768 is max string length
             data, address_server = sock.recvfrom(32768)
 
             # if data is not null
-            if len(data) > 0:
-                # get string number
-                index = int(str(data).replace('b', '')[7:len(str(data))-2])
+            # if len(data) > 0:
+            # get string number
+            index = int(str(data).replace('b', '')[7:len(str(data))-2])
 
-                if index == i+1:  # confirm Hello x = World x
-                    print("Receive", data, "from server")
-                    break
+            if index == i+1:  # confirm Hello x = World x
+                print("Receive", data, "from server")
+                break
 
         except socket.timeout:
             sock.sendto(data, (addr, port_destin))
