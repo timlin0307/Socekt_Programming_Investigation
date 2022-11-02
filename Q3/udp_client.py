eb34e28e5ae8df3afb40c63cd6365ccb3a595298
Yuhung1109 <48060759+Yuhung1109@users.noreply.github.com>
2022-11-03 00:36:12 +0800

    Add files via upload

---
+++ Q3/udp_client.py
@@ -0,0 +1,42 @@
+import socket
+import random
+import time
+
+addr = '127.0.0.1'
+port_self = 5407
+port_destin = 5406
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((addr, port_self))
+# sock.settimeout(0.12)  # at least larger than proxy delay
+
+t_start = time.time()
+
+for i in range(10000):
+    if(i % 2 == 0):
+        sock.settimeout(1)
+    else:
+        sock.settimeout(0.005)
+    data = b'Hello %d' % (i+1)
+    sock.sendto(data, (addr, port_destin))
+    print("Send", data, "to proxy")
+    while True:
+        try:
+            # 32768 is max string length
+            data, address_server = sock.recvfrom(32768)
+
+            # if data is not null
+            if len(data) > 0:
+                # get string number
+                index = int(str(data).replace('b', '')[7:len(str(data))-2])
+
+                if index == i+1:  # confirm Hello x = World x
+                    print("Receive", data, "from server")
+                    break
+
+        except socket.timeout:
+            sock.sendto(data, (addr, port_destin))
+            print("Re-Send", data, "to proxy")
+
+t_end = time.time()
+print("Total running time : ", t_end - t_start, "second")
