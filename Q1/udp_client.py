92519e143a52b77e201f844eafa07911b0a88018
Sasaki Yamato <54052564+timlin0307@users.noreply.github.com>
2022-11-02 15:52:27 +0800

    Add files via upload

--- Q1/udp_client.py
+++ Q1/udp_client.py
@@ -4,18 +4,20 @@ addr = '127.0.0.1'
 port_self = 5407
 port_destin = 5406
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((addr, port_self))
+sock_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock_recv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock_recv.bind((addr, port_self))
+sock_recv.settimeout(0.11)  # at least larger than proxy delay
 
 for i in range(10000):
     data = b'Hello %d' % (i+1)
-    sock.sendto(data, (addr, port_destin))
+    sock_send.sendto(data, (addr, port_destin))
     print("Send", data, "to proxy")
 
     while True:
         try:
             # 32768 is max string length
-            data, address_server = sock.recvfrom(32768)
+            data, address_server = sock_recv.recvfrom(32768)
 
             # if data is not null
             if len(data) > 0:
@@ -27,5 +29,5 @@ for i in range(10000):
                     break
 
         except socket.timeout:
-            sock.sendto(data, (addr, port_destin))
-            print("Re-Send ", data, " to proxy")
+            sock_send.sendto(data, (addr, port_destin))
+            print("Re-Send", data, "to proxy")
--- Q2/udp_client.py
+++ Q2/udp_client.py
@@ -1,25 +1,23 @@
 import socket
-import select
 
 addr = '127.0.0.1'
 port_self = 5407
 port_destin = 5406
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((addr, port_self))
-
-sock.setblocking(0)
-ready = select.select([sock], [], [], 0.001)
+sock_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock_recv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock_recv.bind((addr, port_self))
+sock_recv.settimeout(0.11)  # at least larger than proxy delay
 
 for i in range(10000):
     data = b'Hello %d' % (i+1)
-    sock.sendto(data, (addr, port_destin))
+    sock_send.sendto(data, (addr, port_destin))
     print("Send", data, "to proxy")
 
     while True:
         try:
             # 32768 is max string length
-            data, address_server = sock.recvfrom(32768)
+            data, address_server = sock_recv.recvfrom(32768)
 
             # if data is not null
             if len(data) > 0:
@@ -30,6 +28,6 @@ for i in range(10000):
                     print("Receive", data, "from server")
                     break
 
-        except:
-            sock.sendto(data, (addr, port_destin))
+        except socket.timeout:
+            sock_send.sendto(data, (addr, port_destin))
             print("Re-Send", data, "to proxy")
