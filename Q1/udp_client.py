b5849b870177531808b19e7d3179964f3afce9fa
Sasaki Yamato <54052564+timlin0307@users.noreply.github.com>
2022-11-02 13:33:27 +0800

    Add files via upload

---
+++ Q1/udp_client.py
@@ -0,0 +1,30 @@
+import socket
+
+addr = '127.0.0.1'
+port_self = 5407
+port_destin = 5406
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((addr, port_self))
+
+for i in range(10000):
+    data = b'Hello %d' % (i+1)
+    sock.sendto(data, (addr, port_destin))
+    print("Send ", data, " to proxy")
+
+    while True:
+        try:
+            # 32768 is max string length
+            data, address_server = sock.recvfrom(32768)
+
+            if len(data) > 0:
+                # get string number
+                index = int(str(data).replace('b', '')[7:len(str(data))-2])
+
+                if index == i+1:
+                    print("Receive ", data, " from server")
+                    break
+
+        except socket.timeout:
+            sock.sendto(data, (addr, port_destin))
+            print("Re-Send ", data, " to proxy")
